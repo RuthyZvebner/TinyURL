873e77f6b402baf70e7bab21894decdfb3065027
unknown <r0556721978@gmail.com>
2024-05-31 12:00:01 +0300

    second commit

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -1,4 +1,5 @@
 import linkModel from "../models/linkModel.js";
+import userModel from "../models/userModel.js";
 
 const LinkController = {
     getList: async (req, res) => {
@@ -7,28 +8,53 @@ const LinkController = {
             res.json({ links })
         }
         catch (e) {
-            res.status(400).json({ massage: e.massage })
+            res.status(400).json({ message: e.message })
         }
     },
 
     getById: async (req, res) => {
         try {
             const link = await linkModel.findById(req.params.id)
-            res.json({ link })
+            if (!link)
+                res.status(400).json({ message: 'Link not found' })
+            link.clicks.push({ipAddress:req.ip})
+
+            await link.save()
+            
+            res.redirect(link.originUrl)
         }
         catch (e) {
-            res.status(400).json({ massage: e.massage })
+            res.status(400).json({ message: e.message })
         }
     },
 
     add: async (req, res) => {
-        const { originUrl } = req.body
+        const { originUrl, userId } = req.body
+        //אם לא קבל את כל הפרמטרים
+        if (!originUrl || !userId)
+            return res.status(400).json({ message: 'Missing url or user id' })
         try {
-            const newLink = await linkModel.create({originUrl})
-            res.json({ newLink })
+            const user = await userModel.findById(userId).populate('links')
+            console.log(user._id)
+
+            if (!user)
+                return res.status(400).json({ message: 'User not found' })
+
+            const existUrl = user.links.find(link => link.originUrl === originUrl)
+
+            if (existUrl)
+                return res.json(`http://localhost:3000/links/${existUrl._id}`)
+
+            const newLink = await linkModel.create({ originUrl })
+            user.links.push(newLink)
+            //console.log(user.links)
+            await user.save()
+
+            const shortUrl = `${req.protocol}://${req.get('host')}/links/${newLink._id}`
+            return res.json(shortUrl)
         }
         catch (e) {
-            res.status(400).json({ massage: e.massage })
+            res.status(400).json({ message: e.message })
         }
     },
 
@@ -43,7 +69,7 @@ const LinkController = {
             res.json({ updateLink })
         }
         catch (e) {
-            res.status(400).json({ massage: e.massage })
+            res.status(400).json({ message: e.message })
         }
     },
 
@@ -54,7 +80,7 @@ const LinkController = {
             res.json({ deleteLink })
         }
         catch (e) {
-            res.status(400).json({ massage: e.massage })
+            res.status(400).json({ message: e.message })
         }
     }
 }
--- models/linkModel.js
+++ models/linkModel.js
@@ -3,6 +3,16 @@ const LinkSchema = mongoose.Schema({
     originUrl: {
         type: String,
         default: ''
-    }
+    },
+    clicks: [{
+        insertedAt: {
+            type: Date,
+            default: Date.now()
+        },
+        ipAddress: {
+            type: String,
+            default: "0.0.0.0"
+        }
+    }]
 })
 export default mongoose.model("link", LinkSchema)
